f4f14b809919a655c4abee69d4154c1175ae6743
Kirill <cs1@list.ru>
2022-11-13 12:31:30 +0300

    download book covers

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import requests
 from pathlib import Path
 from itertools import count
 from bs4 import BeautifulSoup
-from pathvalidate import sanitize_filename, sanitize_filepath
+from pathvalidate import sanitize_filename
+from urllib.parse import unquote, urljoin, urlsplit
 
 
 def check_for_redirect(response: requests.Response):
@@ -19,7 +20,23 @@ def get_book_name(url: str) -> str:
     soup = BeautifulSoup(markup=response.text, features='lxml')
     book_tag = soup.find('div', id='content').find('h1')
     book_name, book_author = book_tag.text.strip().split(' \xa0 :: \xa0 ')
+    book_img_url = soup.find('div', class_='bookimage').find('img')['src']
+
+    download_image(urljoin(url, book_img_url))
+
     return book_name
+
+
+def download_image(url: str, folder: str = 'images/'):
+    response = requests.get(url)
+    response.raise_for_status()
+
+    f_name = unquote(urlsplit(url).path.split('/')[-1])
+    f_path = Path(folder, f_name)
+    Path(folder).mkdir(parents=True, exist_ok=True)
+
+    with open(f_path, 'wb') as file:
+        file.write(response.content)
 
 
 def download_txt(url: str, filename: str, folder: str = 'books/') -> str:
