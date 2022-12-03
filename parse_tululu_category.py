0aa10ddce84719c17eeb3d1f36c0102c7592ac92
Kirill Rudenko <cs1@list.ru>
2022-12-03 15:38:06 +0300

    add additional arguments reading from cmd

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -1,6 +1,9 @@
 import argparse
 from itertools import count
 import json
+import os
+from pathlib import Path
+import sys
 import time
 import requests
 from bs4 import BeautifulSoup
@@ -15,8 +18,9 @@ def get_book_page(url: str) -> bool:
     return response
 
 
-def save_books_as_json(books: list) -> None:
-    with open('books.json', 'w', encoding='utf-8') as json_file:
+def save_books_as_json(books: list, folder: str = 'json/') -> None:
+    path = Path(folder, 'books.json')
+    with open(path, 'w', encoding='utf-8') as json_file:
         json.dump(books, json_file, indent=4, ensure_ascii=False)
 
 
@@ -48,21 +52,36 @@ def create_argparser() -> argparse.Namespace:
     parser.add_argument('--end_page',
                         help='Конец диапазона скачиваемых страниц',
                         type=int)
+    parser.add_argument(
+        '--dest_folder',
+        help='Путь к каталогу с результатами парсинга: картинкам, книгам, JSON.',
+        type=str,
+        default='books/')
+    parser.add_argument('--json_path',
+                        help='Указать свой путь к *.json файлу с результатами.',
+                        type=str)
+    parser.add_argument('--skip_imgs',
+                        help='Не скачивать картинки.',
+                        action='store_true')
+    parser.add_argument('--skip_txt', help='Не скачивать книги.', action='store_true')
     return parser.parse_args()
 
 
 def main():
-    args = create_argparser()
     connection_timeout = 10
     sci_fi_category_id = 55
     book_download_url = f'https://tululu.org/txt.php'
     genre_page_url_template = 'https://tululu.org/l{category}/{page}/'
-    book_collection = []
+
+    args = create_argparser()
+    if not args.json_path:
+        args.json_path = args.dest_folder
 
     if not args.end_page:
         args.end_page = get_all_category_pages(genre_page_url_template,
                                                sci_fi_category_id)
 
+    book_collection = []
     for page in range(args.start_page, args.end_page):
         genre_page_url = genre_page_url_template.format(category=sci_fi_category_id,
                                                         page=page)
@@ -84,16 +103,26 @@ def main():
                             book_id = ''.join(
                                 char for char in book_url_slug if char.isdigit())
                             book_filename = f"{book_id}. {book['name']}"
-                            book_file_path = download_txt(book_download_url, book_id,
-                                                          book_filename)
-                            download_image(urljoin(book_page_url, book['img_url']))
-                            book['book_path'] = book_file_path
 
+                            book_file_path = ''
+                            if not args.skip_txt:
+                                book_file_path = download_txt(
+                                    book_download_url, book_id, book_filename,
+                                    args.dest_folder)
+
+                            if not args.skip_imgs:
+                                download_image(urljoin(book_page_url, book['img_url']),
+                                               args.dest_folder)
+
+                            book['book_path'] = book_file_path
                             book_collection.append(book)
 
                     except requests.HTTPError:
                         print(f'\nНеправильная ссылка на книгу: {book_page_url}')
 
+        except KeyboardInterrupt:
+            print(f'Остановлено пользователем.')
+            sys.exit(0)
         except requests.HTTPError:
             print(f'\nНеправильная ссылка на категорию: {genre_page_url}')
         except requests.ConnectionError:
@@ -102,7 +131,7 @@ def main():
         except requests.ReadTimeout:
             print(f'\nВремя ожидания запроса истекло.')
 
-    save_books_as_json(book_collection)
+    save_books_as_json(book_collection, args.json_path)
 
 
 if __name__ == '__main__':
